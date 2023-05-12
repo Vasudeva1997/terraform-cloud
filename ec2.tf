c5f6fabc4105655a0dccf1b3a32c0425c3ac5f54
Vasudeva Gudimalla <34068556+Vasudeva1997@users.noreply.github.com>
2023-05-12 15:46:10 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-# provider "aws" {
-#   region     = "us-east-1"
+provider "aws" {
+  region     = "us-east-1"
 #   access_key = var.ACCESS_KEY
 #   secret_key = var.SECRET_KEY
-# }
+}
 
 resource "aws_instance" "my_ec2" {
   ami                    = "ami-06a0cd9728546d178"
